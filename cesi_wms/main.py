01c05d12d7c31d686d4cd26dbfbd4590f7ec5682
quanpower <quanpower@gmail.com>
2021-01-13 12:12:16 +0800

    add order list post api

--- cesi_wms/main.py
+++ cesi_wms/main.py
@@ -5,22 +5,104 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
+class OrderDetail(BaseModel):
+    seq: str
+    customerCode: str
+    customerName: str
+    productCode: str
+    productName: str
+    signType: int
+    signValue: str
+    materialList: list = []
+
+
+class Order(BaseModel):
+    orders: list = []
+
+order_sample={
+    "code":0,
+    "message":"string",
+    "data":[
+        {
+            "seq":"PP000001",
+            "customerCode":"SC0001",
+            "customerName":"张三",
+            "productCode":"A01",
+            "productName":"白色电池款",
+            "signType":2,
+            "signValue":"http:xxx.jpeg",
+            "materialList":[
+                {
+                    "materialCode":"Za01.01",
+                    "materialName":"白色底壳"
+                },
+                {
+                    "materialCode":"Za02.01",
+                    "materialName":"中壳"
+                },
+                {
+                    "materialCode":"Za03.01",
+                    "materialName":"白色上壳"
+                },
+                {
+                    "materialCode":"Ba01.01",
+                    "materialName":"电池"
+                },
+                {
+                    "materialCode":"Ba02.01",
+                    "materialName":"电池盖"
+                },
+                {
+                    "materialCode":"La01.01",
+                    "materialName":"公制螺丝"
+                }
+            ]
+        },
+        {
+            "seq":"PP000002",
+            "customerCode":"SC0002",
+            "customerName":"李四",
+            "productCode":"B01",
+            "productName":"白色充电款",
+            "signType":1,
+            "signValue":"开心",
+            "materialList":[
+                {
+                    "materialCode":"Za01.01",
+                    "materialName":"白色底壳"
+                },
+                {
+                    "materialCode":"Za02.01",
+                    "materialName":"中壳"
+                },
+                {
+                    "materialCode":"Za03.01",
+                    "materialName":"白色上壳"
+                },
+                {
+                    "materialCode":"La01.01",
+                    "materialName":"公制螺丝"
+                }
+            ]
+        }
+    ]
+}
 
 
 @app.put("/v1/api/order/produce/list")
-def update_order_list():
+def update_order_list(order: Order):
+    orders = order.orders
+    print(orders)
+    seq_list = [i['seq'] for i in orders]
+    print(seq_list)
     return {
         "code" : 0,
         "message" : "数据处理成功！",
-        "data" : []
+        "data" : seq_list
     }
 
 @app.get("/v1/api/wms/warehouse/snapshot/list")
-def get_warehouse_snapshot(q: Optional[str] = None):
+def get_warehouse_snapshot(ver: Optional[int] = None):
     return_json = {
         "code" : 0,
         "message" : "数据处理成功！",
@@ -100,7 +182,7 @@ def get_warehouse_bin(bin_id: int, q: Optional[str] = None):
     return return_json
 
 @app.get("/v1/api/wms/line_storage/snapshot/list")
-def get_line_storage_snapshot(q: Optional[str] = None):
+def get_line_storage_snapshot(ver: Optional[int] = None):
     return_json = {
         "code" : 0,
         "message" : "数据处理成功！",
