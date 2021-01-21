4168b252f1c8af5a81cc6f9393404e17c155660b
quanpower <quanpower@gmail.com>
2021-01-21 16:52:10 +0800

    delete some py

--- plc/RobotActionLock.py
+++ plc/RobotActionLock.py
@@ -57,7 +57,7 @@ def out_action(siemens_1500, positionByte,noByte,quantityByte, enableByte, enabl
                     print(enableBit)
                     print(enable)
 
-                time.sleep(10)
+                time.sleep(3)
 
                 # 出库使能复位
                 with glock:
@@ -75,3 +75,67 @@ def out_action(siemens_1500, positionByte,noByte,quantityByte, enableByte, enabl
 
     logger.info('---out action---end-------')
     print('---out action---end-------')
+
+
+def load_action(siemens_1500, positionByte,noByte,quantityByte, enableByte, enableBit, enable, out_list, glock):
+    logger.info('---out action----start-----')
+    print('---out action----start-----')
+    print(out_list)
+    for out in out_list:
+        i = 0
+        while 1: 
+            print(datetime.datetime.now())
+            position = out['position']
+            no = out['no']
+            quantity = out['quantity']
+
+            if gloVar.ready_ok:
+                print('=====output no=====')
+                print(position)
+                with glock:
+                    siemens_1500.write_int_to_plc(38, positionByte, position)
+                    time.sleep(0.1)
+                    siemens_1500.write_int_to_plc(38, noByte, no)
+                    time.sleep(0.1)
+                    siemens_1500.write_int_to_plc(38, quantityByte, quantity)
+                    time.sleep(0.1)
+                    siemens_1500.write_bool_to_plc(38, enableByte, enableBit, enable)
+                    print(positionByte)
+                    print(enableByte)
+                    print(enableBit)
+                    print(enable)
+
+                time.sleep(3)
+
+                # 出库使能复位
+                with glock:
+                    siemens_1500.write_bool_to_plc(38, enableByte, enableBit, 0)
+                logger.info(position)
+                break
+
+            i += 1
+            print(i)
+            # 跳出while,结束出库
+            if i >= 600:
+                siemens_1500.write_bool_to_plc(38, enableByte, enableBit, 0)
+                return
+            time.sleep(1)
+
+    logger.info('---out action---end-------')
+    print('---out action---end-------')
+
+
+def unload_action(siemens_1500, positionByte, position, enableByte, enableBit, enable, glock):
+    logger.info('---in action----start-----')
+    print('---in action----start-----')
+    print(datetime.datetime.now())
+    with glock:
+        siemens_1500.write_int_to_plc(38, positionByte, position)
+        time.sleep(0.1)
+        siemens_1500.write_bool_to_plc(38, enableByte, enableBit, enable)
+    logger.info(position)
+    logger.info('---in action---end-------')
+    print('---in action---end-------')
+    # while True:
+    #     pass
+
